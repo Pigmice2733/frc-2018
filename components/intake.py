243b1379523d2deda6fddea21734237f57361c18
Franklin Harding <franklinharding0.0@gmail.com>
2018-01-11 18:23:35 -0800

    Added Intake Component

---
+++ components/intake.py
@@ -0,0 +1,30 @@
+import wpilib
+import enum
+import ctre
+from ctre import CANTalon
+
+
+class Action(enum.Enum):
+    Intake = 1
+    Stop = 0
+    Outtake = -1
+
+
+def mirror(l, r, val):
+    l.set(val)
+    r.set(-val)
+
+
+class Intake:
+    l_intake_motor = CANTalon
+    r_intake_motor = CANTalon
+
+    cur_action = Action.Stop
+
+    def execute(self):
+        if self.cur_action == Action.Intake:
+            mirror(self.l_intake_motor, self.r_intake_motor, 1.0)
+        elif self.cur_action == Action.Outtake:
+            mirror(self.l_intake_motor, self.r_intake_motor, -1.0)
+        elif self.cur_action == Action.Stop:
+            mirror(self.l_intake_motor, self.r_intake_motor, 0.0)
--- robot.py
+++ robot.py
@@ -3,17 +3,23 @@
 import wpilib
 from ctre.cantalon import CANTalon
 from magicbot import MagicRobot
+import ctre
 
 from components.drivetrain import Drivetrain
+from components.intake import Intake, Action as IntakeAction
 
 
 class Robot(MagicRobot):
 
+    intake = Intake
     drivetrain = Drivetrain
 
     def createObjects(self):
         self.robot_drive = wpilib.RobotDrive(
             0, 1, 2, 3, motorController=CANTalon)
+
+        self.l_intake_motor = CANTalon(4)
+        self.r_intake_motor = CANTalon(5)
 
         self.drive_joystick = wpilib.Joystick(0)
         self.operator_joystick = wpilib.Joystick(1)
@@ -22,6 +28,16 @@ class Robot(MagicRobot):
         self.drivetrain.turn_at(
             self.drive_joystick.getRawAxis(0), squaredInputs=True)
         self.drivetrain.forward_at(self.drive_joystick.getRawAxis(1))
+
+        action = IntakeAction.Stop
+
+        if self.drive_joystick.getRawButton(1):
+            action = IntakeAction.Outtake
+
+        if self.drive_joystick.getRawButton(2):
+            action = IntakeAction.Intake
+
+        self.intake.cur_action = action
 
 
 if __name__ == '__main__':
