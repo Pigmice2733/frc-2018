a740d54d806981a633cb730d635f8c6f84d72df2
Pigmice <pigmice@localhost.localdomain>
2018-01-11 16:42:12 -0800

    Add Climber

--- robot.py
+++ robot.py
@@ -5,15 +5,18 @@ from ctre.cantalon import CANTalon
 from magicbot import MagicRobot
 
 from components.drivetrain import Drivetrain
-
+from components.climber import Climber
 
 class Robot(MagicRobot):
 
     drivetrain = Drivetrain
+    climber = Climber
 
     def createObjects(self):
         self.robot_drive = wpilib.RobotDrive(
             0, 1, 2, 3, motorController=CANTalon)
+
+        self.climber_motor = CANTalon(4)
 
         self.drive_joystick = wpilib.Joystick(0)
         self.operator_joystick = wpilib.Joystick(1)
@@ -22,6 +25,8 @@ class Robot(MagicRobot):
         self.drivetrain.turn_at(
             self.drive_joystick.getRawAxis(0), squaredInputs=True)
         self.drivetrain.forward_at(self.drive_joystick.getRawAxis(1))
+        if self.operator_joystick.getRawButton(10):
+            self.climber.climb()
 
 
 if __name__ == '__main__':
