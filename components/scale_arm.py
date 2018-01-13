7891afc9e7111f2f324248ab1a2a0019f5e785af
Orion Capell <orion.capell@gmail.com>
2018-01-12 20:39:30 -0800

    Added initial scale_arm.py

---
+++ components/scale_arm.py
@@ -0,0 +1,25 @@
+import wpilib
+from ctre.cantalon import CANTalon
+from enum import Enum
+
+class State(Enum):
+    REST = 1
+    UP = 2
+    DOWN = 3
+
+class ScaleArm:
+    scale_arm_motor = CANTalon
+    state = State.REST
+    
+    def up(self):
+        self.state = State.UP
+    def down(self):
+        self.state = State.DOWN
+    def execute(self):
+        if self.state == State.UP:
+            self.scale_arm_motor.set(0.2)
+        elif self.state == State.DOWN:
+            self.scale_arm_motor.set(-0.2)
+        else:
+            self.scale_arm_motor.set(0)
+        self.state = State.REST
