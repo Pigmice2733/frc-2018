3f8feb2624bbe724712b8af2c0583f44f934d129
Caleb Eby <calebeby@users.noreply.github.com>
2018-01-20 15:45:31 -0800

    Update to 2018 wpilib (#8)
    * Update to 2018 wpilib
    * Switch to using WPI_TalonSRX for compatibility

--- components/climber.py
+++ components/climber.py
@@ -1,19 +1,16 @@
-import wpilib
-from ctre.cantalon import CANTalon
+from ctre.wpi_talonsrx import WPI_TalonSRX
 
 
 class Climber:
-    climber_motor = CANTalon
+    climber_motor = WPI_TalonSRX
     climbing = False
 
     def climb(self):
         self.climbing = True
-           
+
     def execute(self):
         if self.climbing:
             self.climber_motor.set(0.2)
         else:
             self.climber_motor.set(0)
         self.climbing = False
-
-        
