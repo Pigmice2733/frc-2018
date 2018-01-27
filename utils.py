5b899c30a5cd664bbfac678d55390886a4f9ad3a
Brendan Burkhart <blackravenbrendan@gmail.com>
2018-01-27 01:17:38 -0800

    Fix key/path error in NetworkTablesStreamer

--- utils.py
+++ utils.py
@@ -9,19 +9,19 @@ class NetworkTablesStreamer:
         """value must be a tuple"""
         if isinstance(value, tuple):
             for key in value._fields:
-                self._send_value(key, getattr(value, key), name)
+                self._send_value(key, getattr(value, key), name + "/")
         elif isinstance(value, list):
-            self._send_value(key, value, name)
+            self._send_value("", value, name)
 
     def _send_value(self, key, value, path):
         if isinstance(value, tuple):
             for key in value._fields:
                 self._send_value(key, getattr(value, key), path)
         elif isinstance(value, int) or isinstance(value, float):
-            self.table.putNumber(path + "/" + key, value)
+            self.table.putNumber(path + key, value)
         elif isinstance(value, bool):
-            self.table.putBoolean(path + "/" + key, value)
+            self.table.putBoolean(path + key, value)
         elif isinstance(value, str):
-            self.table.putString(path + "/" + key, value)
+            self.table.putString(path + key, value)
         else:
-            self.table.putString(path + "/" + key, json.dumps(value))
+            self.table.putString(path + key, json.dumps(value))
