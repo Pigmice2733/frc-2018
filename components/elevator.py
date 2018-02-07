007c336ac27711f493815c07494456c2cf6b573e
Brendan Burkhart <blackravenbrendan@gmail.com>
2018-02-06 22:51:16 -0800

    Add elevator controls

--- components/elevator.py
+++ components/elevator.py
@@ -1,28 +1,38 @@
-import wpilib
 from ctre.wpi_talonsrx import WPI_TalonSRX
-
-from motioncontrol import pid
 
 
 class Elevator:
     winch = WPI_TalonSRX
-    goal_positions = [0, 50, 150]
+    goal_positions = [0, 4, 10]
     goal_index = goal_positions[0]
 
     def setup(self):
-        self.winch.setQuadraturePosition(0)
-        self.winch.configSelectedFeedbackSensor(WPI_TalonSRX.FeedbackDevice.QuadEncoder)
+        self.winch.configSelectedFeedbackSensor(
+            WPI_TalonSRX.FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0)
 
-    def inc_goal(self):
-        """Move position up to a higher goal position"""
+        self.winch.configNominalOutputForward(0, 0)
+        self.winch.configNominalOutputReverse(0, 0)
+        self.winch.configPeakOutputForward(1, 0)
+        self.winch.configPeakOutputReverse(-1, 0)
+
+        self.winch.config_kP(0, 0.1, 0)
+        self.winch.config_kI(0, 0.0, 0)
+        self.winch.config_kD(0, 0.0, 0)
+        self.winch.config_kF(0, 0.0, 0)
+
+        self.winch.configAllowableClosedloopError(0, 0, 0)
+
+        self.winch.setSelectedSensorPosition(0, 0, 0)
+
+    def raise_goal(self):
         self.goal_index += 1
         self.goal_index = min(self.goal_index, len(self.goal_positions - 1))
 
-    def dec_goal(self):
-        """Move position down to a lower goal position"""
+    def lower_goal(self):
         self.goal_index -= 1
         self.goal_index = max(self.goal_index, len(self.goal_positions) - 1)
 
     def execute(self):
-        target_position = self.goal_positions[self.goal_index]
+        # Get goal position in revolutions, scale to ticks - MagEncoder is a 1024 cpr quad
+        target_position = self.goal_positions[self.goal_index] * 1024 * 4
         self.winch.set(WPI_TalonSRX.ControlMode.Position, target_position)
