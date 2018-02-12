4400343e636aad90e9c939fcef8d01a900f93e9a
Brendan Burkhart <blackravenbrendan@gmail.com>
2018-02-11 17:03:32 -0800

    Properly set starting position in switch auto

--- autonomous/switch.py
+++ autonomous/switch.py
@@ -31,11 +31,11 @@ class SwitchAutonomous(AutonomousStateMachine):
     ]
 
     left_position = RobotState(
-        position=Point(0.72 / 2, 0.84 / 2), rotation=math.pi / 2
+        position=Point(0.76 + 0.89 / 2, 1.01 / 2), rotation=math.pi / 2
     )
 
     right_position = RobotState(
-        position=Point(8.23 - 0.72 / 2, 0.84 / 2), rotation=math.pi / 2)
+        position=Point(8.23 - 0.76 - 0.89 / 2, 1.01 / 2), rotation=math.pi / 2)
 
     def __init__(self):
         initial_states = [('left', self.left_position), ('right', self.right_position)]
@@ -49,41 +49,23 @@ class SwitchAutonomous(AutonomousStateMachine):
             self.MODE_NAME, 'right', initial_states, waypoints)
 
     def initialize_path(self):
-        initial_robot_state = RobotState(
-            position=Point(8.23 - 0.76 - 0.89 / 2, 1.01 / 2), rotation=math.pi / 2)
-
-        # Robot width = 0.89m
-        # Robot height = 0.1.01m
-        # Side to middle of switch plate = 2.62m
-        # Portal x width = 0.76m
-        # Alliance station wall to edge of switch plate = 3.74m
-        # Alliance station wall to close edge of scale platform = 6.65m
-
-        near_side_waypoints = [
-            Point(8.23 - 0.762 - (2.62 / 3), 3.74 / 4.5),
-            Point(8.23 - 2.62 + 0.05, 3.74 / 3.3),
-            Point(8.23 - 2.62, 3.74 - (0.84 / 2))
-        ]
-
-        far_side_waypoints = [
-            Point(8.23 - 0.48, 5.20),
-            Point(8.23 - 1.5, 6.05),
-            Point(2.16, 6.00),
-            Point(1.44, 6.00),
-            Point(0.8, 5.36),
-            Point(0.8, 4.45),
-            Point(2.16 - 1.01 / 2 - 0.2, 4.45)
-        ]
-
-        path = Path(initial_robot_state,
-                    1.1,
-                    0,
-                    1.4,
-                    1.2,
-                    False,
-                    waypoints=far_side_waypoints)
-        # path = Path(initial_robot_state, math.pi / 2, 1.3, 1.5,
-        #             False, 1.5, waypoints=near_side_waypoints)
+        if Selector.starting_position == 'left':
+            waypoints = Selector.mirror_waypoints(self.right_far_side_waypoints, 8.23)
+            path = Path(self.left_position,
+                        1.1,
+                        0,
+                        1.4,
+                        1.2,
+                        False,
+                        waypoints=waypoints)
+        else:
+            path = Path(self.right_position,
+                        1.1,
+                        0,
+                        1.4,
+                        1.2,
+                        False,
+                        waypoints=self.right_far_side_waypoints)
 
         self.drivetrain.set_path(path)
 
