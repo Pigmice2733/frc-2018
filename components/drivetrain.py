c646f2c7e67d3274f3be2da997e2f36eddea34c3
user <user@spammail.com>
2018-03-03 17:16:16 -0800

    Get far side scale auto working

--- components/drivetrain.py
+++ components/drivetrain.py
@@ -6,8 +6,9 @@ from wpilib import Compressor, drive
 
 from motioncontrol.execution import PathTracker
 from motioncontrol.path import Path
-from motioncontrol.utils import (Completed, RobotCharacteristics, RobotState, interpolate,
-                                 tank_drive_odometry, tank_drive_wheel_velocities)
+from motioncontrol.utils import (Completed, RobotCharacteristics, RobotState,
+                                 interpolate, tank_drive_odometry,
+                                 tank_drive_wheel_velocities)
 
 from utils import NetworkTablesSender
 
@@ -65,7 +66,8 @@ class Drivetrain:
             max_speed=max_speed,
             wheel_base=self.robot_characteristics.wheel_base,
             encoder_ticks=self.robot_characteristics.encoder_ticks,
-            revolutions_to_distance=self.robot_characteristics.revolutions_to_distance,
+            revolutions_to_distance=self.robot_characteristics.
+            revolutions_to_distance,
             speed_scaling=self.robot_characteristics.speed_scaling)
 
         self.path_tracker = PathTracker(
@@ -86,28 +88,31 @@ class Drivetrain:
     def get_odometry(self) -> RobotState:
         return self.robot_state
 
-    def _get_orientation(self) -> float:
+    def get_orientation(self) -> float:
         return math.radians(-self.navx.getAngle())
 
     def _set_orientation(self, orientation) -> float:
         self.navx.setAngleAdjustment(0)
-        self.navx.setAngleAdjustment(-self.navx.getAngle() - math.degrees(orientation))
+        self.navx.setAngleAdjustment(
+            -self.navx.getAngle() - math.degrees(orientation))
 
     def _update_odometry(self):
         encoder_scaling = (self.robot_characteristics.encoder_ticks /
                            self.robot_characteristics.revolutions_to_distance)
 
-        current_wheel_distances = (-self.left_drive_motor.getQuadraturePosition() / encoder_scaling,
-                                   self.right_drive_motor.getQuadraturePosition() / encoder_scaling)
+        current_wheel_distances = (
+            -self.left_drive_motor.getQuadraturePosition() / encoder_scaling,
+            self.right_drive_motor.getQuadraturePosition() / encoder_scaling)
 
         enc_velocity = (self.right_drive_motor.getQuadratureVelocity() -
                         self.left_drive_motor.getQuadratureVelocity()) / 2
 
         velocity = 10 * enc_velocity / encoder_scaling
 
-        self.robot_state = tank_drive_odometry(current_wheel_distances, self.wheel_distances,
-                                               self._get_orientation(), self.robot_state.rotation,
-                                               self.robot_state.position, velocity)
+        self.robot_state = tank_drive_odometry(
+            current_wheel_distances, self.wheel_distances,
+            self.get_orientation(), self.robot_state.rotation,
+            self.robot_state.position, velocity)
 
         self.wheel_distances = current_wheel_distances
 
@@ -130,8 +135,9 @@ class Drivetrain:
                 scale = interpolate(0.35, 1, 0, 0.9, radius)
                 scale = min(scale, 1.0)
                 self.forward *= scale
-            v_left, v_right = tank_drive_wheel_velocities(self.robot_characteristics.wheel_base,
-                                                          self.forward, self.curvature)
+            v_left, v_right = tank_drive_wheel_velocities(
+                self.robot_characteristics.wheel_base, self.forward,
+                self.curvature)
             self.left, self.right = self._scale_speeds(v_left, v_right)
 
         self.robot_drive.tankDrive(self.left, self.right, squaredInputs=False)
